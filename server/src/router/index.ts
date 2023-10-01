a07d04cdd729403cdfe3cd1e4a028557de2c270e
ahmedragab <ahmedragab20901@gmail.com>
2023-10-01 16:43:04 +0300

    ..

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -1,6 +1,14 @@
 import { Express } from "express";
 import productsRouter from "./products.router";
-
+import { cpus, availableParallelism } from "node:os";
 export default (app: Express) => {
   app.use("/products", productsRouter);
+
+  app.get("/cpus", (request, response) => {
+    const cpusDetails = cpus();
+
+    response.json({
+      cpusDetails,
+    });
+  });
 };
